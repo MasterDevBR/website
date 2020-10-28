1415c9d4d2a2a20b65db48e5c637f801fbc421d8
Alberto Trevisan <mineplay152@gmail.com>
2020-10-28 06:01:58 -0300

    importando index.php

---
+++ index.php
@@ -0,0 +1,85 @@
+<head>
+	<style>
+		.orange { color: #ED5100; }
+		.white { color: #FFFFFF; }
+		.yellow { color: #FFFF00; }
+		.bg-orange { background-color: #ED5100; }
+	</style>
+	<link href="https://fonts.googleapis.com/css2?family=Exo+2:wght@400;600&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@700&display=swap" rel="stylesheet">
+</head>
+<body style='margin:0;font-family:"Exo 2", sans-serif;font-weight:600;'>
+<section id="inicio" style="background: url('onibus_na_montanha.jpg');width:100%;height:100vh;background-size:cover;font-size:xx-large">
+<h1 class='white' style='text-transform: uppercase;padding:250px'>O mundo em conectividade móvel</h1>
+</section>
+<section id="necessidade" style="background: url('passageira_de_pe.jpg');width:100%;height:100vh;background-size:cover;">
+	<div style="float:right;padding: 0px 50px;width:30%;background:#000000D9;height: 100%" class='white'>
+		<br><br><br><br>
+		<h2 class='orange'>Necessidade</h2>
+		<p>Em um mundo onde a <b class='orange'>concorrência aumenta a cada dia</b>, é necessário utilizarmos a tecnologia de maneira diferenciada.</p>
+		<p>Sabendo disso, inúmeras empresas <b class='white'>oferecem Wi-Fi grauito</b>, como forma de atrair mais consumidores e <b class='orange'>alavancar resultados positivos</b>.</p>
+		<p>Além de <b class='orange'>aumentar o fluxo de clientes</b>, usando uma tecnologia moderna, é possível <b class='orange'>coletar dados para projetos internos</b> da empresa.</p>
+	</div>
+</section>
+<section id="gestao_wifi" style="background: url('onibus_poste.jpg');width: 100%;height: 100vh;background-size:cover">
+	<div style="float:left;width:30%;height:100%;padding: 0px 50px" class='bg-orange white'>
+		<br><br><br>
+		<h2>Gestão InterFi</h2>
+		<p class='yellow'>E se você oferecesse todo esse acesso, tendo total controle sobre suas redes?</p>
+		<p style='font-weight: 400;'>Com a gestão da InterFi, você pode fazer isso de forma fácil e acessível.</p>
+		<p style='font-weight: 400;'>Oferecemos <b>pontos de acesso</b>, <b>equipamentos</b>, <b>relatórios</b>, todo o <b>suporte</b> necessário pré ou pós venda, e ainda o <b>fornecimento de internet</b>.
+		<p style='font-weight: 400;'>Tudo isso com <b>a Qualidade Internacional</b> que a sua empresa merece.</p>
+	</div>
+</section>
+<section id="seguranca" style="background: url('passageiro_mascara.jpg');height:100vh;background-size:cover;">
+	<div style="float:right;width:30%;background:#000000D9;color:white;height:100%;padding: 0px 50px;">
+		<br><br><br>
+		<h2>Segurança</h2>
+		<p>Temos toda nossa plataforma hospedade em nuvem com proteção em servidor escalável da <b class='orange'>Google Cloud</b>.</p>
+		<p>Portanto, trabalhamos com um dos mais confiáveis servidores, de alta performance, disponível no mercado.</p>
+		<p>Plataforma privada com:</p>
+		<ul>
+			<li class='orange'>Acesso restrito</li>
+			<li class='orange'>Firewall</li>
+			<li class='orange'>Chaves com criptografia</li>
+		</ul>
+	</div>
+</section>
+<section id="equipe" style="background: url('passageiro_sentada.jpg');height:55vh;background-size:cover;background-position:center">
+	<div style='background: #000000;opacity:0.75;height:55vh'>
+		<div style='width:30%;display:inline-block;height:55vh;margin-left:20%'>
+			<br><br><br>
+			<h1 class='orange' style='font-size:34px'>EQUIPE</h1>
+			<ul style='text-transform:uppercase' class='white'>
+				<li>Confidencialidade de Informações Sensíveis</li>
+				<li>Experiência e Competência</li>
+				<li>Know Tecnológico</li>
+				<li>Equipametnos de Última Geração</li>
+			</ul>
+		</div>
+		<div style='width:40%;float:right;height:55vh;padding: 0px;'>
+			<div style='width:56px;height:56px;margin-top:26vh'>
+				<a href='https://api.whatsapp.com/send?phone=5515997006414&text=Ol%C3%A1%20Interfi!%20Eu%20gostaria%20de%20mais%20informa%C3%A7%C3%B5es%20sobre%20InterFi' target='_blank'>
+					<img style='width:100%' src="https://iconmonstr.com/wp-content/g/gd/png.php?size=240&padding=0&icon=assets/source/2016/png/iconmonstr-whatsapp-4.png&in=iconmonstr-whatsapp-4.png&bgShape=&bgColorR=200&bgColorG=200&bgColorB=200&iconColorR=237&iconColorG=81&iconColorB=0">
+				</a>
+			</div>
+		</div>
+	</div>
+</section>
+<section id="indispensavel" style="background:white;height:80vh;">
+	<div style='width:calc(55% - 50px);float:left;height:80vh;'>
+		<img style='width:100%;margin-top:10vh;margin-left:50px' src='transito.jpg'>
+	</div>
+	<div style='width:calc(45% - 50px);float:left;height:80vh;'>
+		<div style='margin-top:20vh;margin-left:100px'>
+			<h2 class='orange'>Indispensável</h2>
+			<p>Ofereça Wi-Fi aos passageiros de aplicativo: ônibus, vans e táxis.</p>
+			<a href='contato.php'>
+				<button style='background:white;color:#ed5100;margin-top:15px;font-size:15px;border:#ed5100 2px solid;padding: 10px 15px;font-weight:700;font-family: "Open Sans"'>ENTRE EM CONTATO</button>
+			</a>
+		</div>
+	</div>
+</section>
+<footer style='height:60px;background:black;color:white;'>
+	<p style='line-height: 60px;text-align:center'> COPYRIGHT &copy; 2020 | INTERFI</p>
+</footer>
